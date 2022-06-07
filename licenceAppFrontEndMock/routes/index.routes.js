0aa24a0c0fa877a61123b4d7b9545a235638964a
Paris2083 <102171285+parisbrookerPI@users.noreply.github.com>
2022-06-07 10:58:09 +0100

    ejs loop structre - client 1

--- licenceAppFrontEndMock/routes/index.routes.js
+++ licenceAppFrontEndMock/routes/index.routes.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var router = express.Router();
-
+const fetch = require("node-fetch");
 // GET home page
 router.get("/", async (req, res, next) => {
-  console.log("aight");
-  let ok = "RENDER WORKS";
-  res.render("index", { ok: ok });
+  res.render("index");
 });
 
 router.get("/client1", async (req, res, next) => {
-  console.log("aight");
-  let ok = "RENDER WORKS";
-  res.render("client1", { ok: ok });
+  const {
+    licencedUsers,
+    unlicencedUsers,
+    licenceCounter,
+  } = await userController();
+
+  res.render("client1", { licencedUsers });
 });
 
 router.get("/client2", async (req, res, next) => {
@@ -27,3 +29,13 @@ router.get("/client3", async (req, res, next) => {
 });
 
 module.exports = router;
+
+async function userController() {
+  let request = await fetch("http://localhost:1002/getallusers");
+  let { licencedUsers, unlicencedUsers, licenceCounter } = await request.json();
+  return {
+    licencedUsers,
+    unlicencedUsers,
+    licenceCounter,
+  };
+}
